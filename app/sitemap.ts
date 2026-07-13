d86520f80521279a11656110810a5fba432ce8b3
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-07-13 20:35:05 +0300

    Configure GitHub Pages deployment

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { projects, siteConfig } from "@/lib/data";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticPages = [
     "",
